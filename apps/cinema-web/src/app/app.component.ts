57b8dc309f809accf14d7650192c979e78ceefb4
falagan <ep.falagan@gmail.com>
2021-10-26 23:48:14 +0200

    FEAT Setted basic app skeleton

--- apps/cinema-web/src/app/app.component.ts
+++ apps/cinema-web/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cinema-root',
-  templateUrl: './app.component.html',
+  template: `<router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
