1ed5d15358b214bdfe520f916faecfa3050bae53
falagan <ep.falagan@gmail.com>
2021-10-30 01:13:18 +0200

    FEAT Movies list and card-movie component design.

--- apps/cinema-web/src/app/modules/movies/movies.module.ts
+++ apps/cinema-web/src/app/modules/movies/movies.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RatingModule } from 'primeng/rating';
+import { SharedModule } from '../../common/shared.module';
 import { CardMovieComponent } from './components/card-movie/card-movie.component';
 import { ListMoviesComponent } from './components/list-movies/list-movies.component';
 import { ProfileMovieComponent } from './components/profile-movie/profile-movie.component';
 import { MoviesRoutingModule } from './movies-routing.module';
-import { SkeletonModule } from 'primeng/skeleton';
-import { SharedModule } from '../../common/shared.module';
 
 @NgModule({
   declarations: [ListMoviesComponent, ProfileMovieComponent, CardMovieComponent],
-  imports: [CommonModule, MoviesRoutingModule, MatCardModule, SkeletonModule, SharedModule]
+  imports: [CommonModule, FormsModule, MoviesRoutingModule, MatCardModule, SharedModule, RatingModule, MatIconModule]
 })
 export class MoviesModule {}
