17abbe75e54cdaee456eed3e2aea7a105d976850
falagan <ep.falagan@gmail.com>
2021-10-29 08:44:35 +0200

    FEAT Created new global lib to reuse Store (State management).

---
+++ libs/lib-store/src/lib/lib-store.spec.ts
@@ -0,0 +1,7 @@
+import { libStore } from './lib-store';
+
+describe('libStore', () => {
+  it('should work', () => {
+    expect(libStore()).toEqual('lib-store');
+  });
+});
---
+++ libs/lib-store/src/lib/lib-store.ts
@@ -0,0 +1,3 @@
+export function libStore(): string {
+  return 'lib-store';
+}
