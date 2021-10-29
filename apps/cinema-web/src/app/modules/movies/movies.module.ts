411a6aa142ccb07d7cd988a10947d1d214a517a3
falagan <ep.falagan@gmail.com>
2021-10-29 17:33:22 +0200

    FEAT main skeleton to show loading and movie cards

--- apps/cinema-web/src/app/modules/movies/movies.module.ts
+++ apps/cinema-web/src/app/modules/movies/movies.module.ts
@@ -1,21 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { MoviesRoutingModule } from './movies-routing.module';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { CardMovieComponent } from './components/card-movie/card-movie.component';
 import { ListMoviesComponent } from './components/list-movies/list-movies.component';
 import { ProfileMovieComponent } from './components/profile-movie/profile-movie.component';
-
+import { MoviesRoutingModule } from './movies-routing.module';
+import { SkeletonModule } from 'primeng/skeleton';
+import { SharedModule } from '../../common/shared.module';
 
 @NgModule({
-  declarations: [
-    CardMovieComponent,
-    ListMoviesComponent,
-    ProfileMovieComponent
-  ],
-  imports: [
-    CommonModule,
-    MoviesRoutingModule
-  ]
+  declarations: [ListMoviesComponent, ProfileMovieComponent, CardMovieComponent],
+  imports: [CommonModule, MoviesRoutingModule, MatCardModule, SkeletonModule, SharedModule]
 })
-export class MoviesModule { }
+export class MoviesModule {}
