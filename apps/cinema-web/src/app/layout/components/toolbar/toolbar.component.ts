de5956f0e6cbf702df29d531404e83f92ba3d01d
falagan <ep.falagan@gmail.com>
2021-10-30 12:29:57 +0200

    FEAT Added funcionality to app logout

--- apps/cinema-web/src/app/layout/components/toolbar/toolbar.component.ts
+++ apps/cinema-web/src/app/layout/components/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angul
 import { Observable } from 'rxjs';
 import { GlobalStateProps } from '../../../common/global-state/store/global-initial.state';
 import { GlobalStateService } from './../../../common/global-state/global-state.service';
+import { AuthService } from './../../../common/services/auth.service';
 
 @Component({
   selector: 'cinema-toolbar',
@@ -13,14 +14,18 @@ export class ToolbarComponent {
   @Output() openSideMenu = new EventEmitter();
   public title$: Observable<string>;
 
-  constructor(private globalState: GlobalStateService) {
+  constructor(private globalState: GlobalStateService, private authService: AuthService) {
     this.binds();
   }
 
-  // UTILS
+  // EVENTS
 
   public onOpenSideMenu() {
     this.openSideMenu.emit(true);
+  }
+
+  public onLogout() {
+    this.authService.logout()
   }
 
   // BINDS
