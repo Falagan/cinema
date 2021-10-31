abeab44e4238753fcca8e228099dcc07aacd9fb2
falagan <ep.falagan@gmail.com>
2021-10-31 16:44:29 +0100

    FEAT Basic check routing with e2e test

--- apps/cinema-web-e2e/src/integration/app.spec.ts
+++ apps/cinema-web-e2e/src/integration/app.spec.ts
@@ -1,13 +1,55 @@
-import { getGreeting } from '../support/app.po';
+import {
+  checkMoviesList,
+  getCinemaActionButton,
+  getGreeting,
+  getLogoutButton,
+  getMovieFromMoviesList,
+  getToolBarSection
+} from '../support/app.po';
 
 describe('cinema-web', () => {
-  beforeEach(() => cy.visit('/'));
+  before(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should display logo app', () => {
+    getGreeting().contains('bienvenido a tu cine favorito!');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to cinema-web!');
+  it('should do login and access to Movies List section', () => {
+    cy.login('bruce@mail.com', '123456');
+    cy.wait(2000, { timeout: 2000 });
+    getToolBarSection().contains('Películas');
+    checkMoviesList().find('cinema-card-movie').should('have.length', 10);
+  });
+
+  it('on click new movie should show a new movie form', () => {
+    getCinemaActionButton().click();
+    cy.wait(2000, { timeout: 2000 });
+    getToolBarSection().contains('Nueva película');
+  });
+
+  it('on click go back button in new movie it shoud come back to movies list', () => {
+    getCinemaActionButton().click();
+    cy.wait(2000, { timeout: 2000 });
+    getToolBarSection().contains('Películas');
+    checkMoviesList().find('cinema-card-movie').should('have.length', 10);
+  });
+
+  it('on dblclick on first movie should show the movie profile', () => {
+    getMovieFromMoviesList().dblclick({ timeout: 1000 });
+    cy.wait(2000, { timeout: 2000 });
+    getToolBarSection().contains('Dancing Lady');
+  });
+
+  it('on click go back button in movie profile it shoud come back to movies list', () => {
+    getCinemaActionButton().first().click();
+    cy.wait(2000, { timeout: 2000 });
+    getToolBarSection().contains('Películas');
+    checkMoviesList().find('cinema-card-movie').should('have.length', 10);
+  });
+
+  it('on click logout it should load home screen', () => {
+    getLogoutButton().click();
+    cy.wait(2000, { timeout: 2000 });
+    getGreeting().contains('bienvenido a tu cine favorito!');
   });
 });
--- apps/cinema-web-e2e/src/support/app.po.ts
+++ apps/cinema-web-e2e/src/support/app.po.ts
@@ -1,1 +1,13 @@
-export const getGreeting = () => cy.get('h1');
+export const getGreeting = () => cy.get('h3');
+
+export const getToolBarSection = () => cy.get('span');
+
+export const checkMoviesList = () => cy.get('cinema-list-movies');
+
+export const getCinemaActionButton = () => cy.get('.float-button');
+
+export const getMovieFromMoviesList = () => cy.get('cinema-card-movie').first();
+
+export const getLogoutButton = () => cy.get('button').contains('logout')
+
+
